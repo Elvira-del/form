52b7e1bad320b613c9a520d21ebd943edb4da3d5
Elvira Krutova <77353395+Elvira-del@users.noreply.github.com>
2024-02-20 18:49:41 +0000

    feat: add link ui

---
+++ src/components/link/LinkWrapper.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from "react";
+import Link from "next/link";
+import { Url } from "next/dist/shared/lib/router/router";
+import styles from "./link.module.css";
+
+type LinkType = {
+  reference: Url;
+  children: ReactNode;
+};
+
+export const LinkWrapper = ({ reference, children }: LinkType) => {
+  return <Link href={reference}>{children}</Link>;
+};
