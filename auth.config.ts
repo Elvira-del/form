b254af8fab2e9c9d216db9d4f1ae78674d1daea5
Elvira Krutova <77353395+Elvira-del@users.noreply.github.com>
2024-04-10 21:50:18 +0100

    feat: add middleware for authentication

--- auth.config.ts
+++ auth.config.ts
@@ -5,5 +5,20 @@ export const authConfig = {
     signIn: "/signin",
     newUser: "/signup",
   },
+  callbacks: {
+    authorized({ request, auth }): boolean | Response {
+      const url = request.nextUrl;
+      const user = auth?.user;
+
+      if (url.pathname.startsWith("/account")) {
+        if (user) return true;
+        return false;
+      } else if (!!user) {
+        return Response.redirect(new URL("/account", url));
+      }
+
+      return true;
+    },
+  },
   providers: [],
 } satisfies NextAuthConfig;
