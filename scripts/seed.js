36889c0a922887b7b5fc45e6222937a19a61e17f
Elvira Krutova <77353395+Elvira-del@users.noreply.github.com>
2024-04-23 19:37:50 +0100

    feat: add creating users table in database

--- scripts/seed.js
+++ scripts/seed.js
@@ -1,9 +1,9 @@
 import { sql } from "@vercel/postgres";
 
-async function createUsersTable() {
+async function createUsersTable(client) {
   try {
-    const users = await sql`CREATE TABLE users (
-        user_id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+    const users = await client.sql`CREATE TABLE IF NOT EXISTS users (
+        user_id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
         user_name varchar(255) NOT NULL,
         user_email TEXT NOT NULL UNIQUE,
         user_password TEXT NOT NULL
@@ -17,11 +17,11 @@ async function createUsersTable() {
 }
 
 async function main() {
-  await sql.connect();
+  const client = await sql.connect();
 
-  await createUsersTable();
+  await createUsersTable(client);
 
-  await sql.end();
+  client.release();
 }
 
 main().catch((error) => {
